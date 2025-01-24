3b37712529d3ec9a84a9e5f1de42c5e8abb218d2
Thomas <thomas.balmant@qintess.com>
2025-01-24 17:38:48 -0300

    shorebird configured

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,44 @@
 import 'package:core/app/app_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:shorebird_code_push/shorebird_code_push.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class _MyAppState extends State<MyApp> {
+  final updater = ShorebirdUpdater();
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Get the current patch number and print it to the console.
+    // It will be `null` if no patches are installed.
+    updater.readCurrentPatch().then((currentPatch) {
+      print('The current patch number is: ${currentPatch?.number}');
+    });
+    _checkForUpdates();
+  }
+
+  Future<void> _checkForUpdates() async {
+    // Check whether a new update is available.
+    final status = await updater.checkForUpdate();
+
+    if (status == UpdateStatus.outdated) {
+      try {
+        // Perform the update
+        await updater.update();
+      } on UpdateException catch (error) {
+        // Handle any errors that occur while updating.
+      }
+    }
+  }
 
   // This widget is the root of your application.
   @override
